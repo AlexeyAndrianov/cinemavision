cfc50a43f3ea484c1c6a3481d938ce79063af502
AlexAndrianov <aliaksei.andryianau@smartpension.co.uk>
2022-05-22 18:28:30 +0200

    fixed typos, adjusted components calling

---
+++ src/components/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomepageHeader from './header/HomepageHeader'
+import HomepageBody from './body/HomepageBody'
+import HomepageFooter from './footer/HomepageFooter'
+
+export default function Homepage() {
+  return (
+      <> 
+        <div><HomepageHeader /></div>
+        <div><HomepageBody /></div>
+        <div><HomepageFooter /></div>
+      </>
+  )
+}
---
+++ src/components/homepage/header/HomepageHeader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function HomepageHeader() {
+  return (
+    <div>HomepageHeader</div>
+  )
+}
+
