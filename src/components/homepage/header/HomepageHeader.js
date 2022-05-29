f3c411e39c6e30ee5ce44ae021ecf73016fc4ba8
AlexAndrianov <aliaksei.andryianau@smartpension.co.uk>
2022-05-29 20:56:17 +0200

    added search group and styles for it

--- src/components/homepage/header/HomepageHeader.js
+++ src/components/homepage/header/HomepageHeader.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import SearchInput from './SearchInput'
+import SearchButton from './SearchButton'
 
 export default function HomepageHeader() {
   return (
     <div>
-      <img src="https://static.alphacoders.com/thumbs_categories/20.jpg"></img>
+      <div>
+        <img src="https://static.alphacoders.com/thumbs_categories/20.jpg"></img>
+      </div>
+      <div className="search_group">
+        <SearchInput />
+        <SearchButton />
+      </div>
     </div>
   )
 }
--- src/components/styles/HomepageHeaderContainer.styled.js
+++ src/components/styles/HomepageHeaderContainer.styled.js
@@ -1,16 +1,20 @@
 import styled from 'styled-components';
 
 export const HomepageHeaderContainer = styled.div`
-  div {
-    width: 1200;
-    height: 396;
-  }
-
   img {
     width: 1200px;
     height: 396px;
     object-fit: cover;
     opacity: 0.6;
     -webkit-filter: blur(3px);
+  }
+
+  .search_group {
+    width: 220px;
+    height: 25px;
+    margin: 0 auto;
+    padding: 10px;
+    top: 50%;
+    left: 50%;
   }
 `
