b9996ad828d6b684607ea5adaeee4ec6d9653c38
AlexAndrianov <aliaksei.andryianau@smartpension.co.uk>
2022-05-30 14:15:35 +0200

    adjusted styled of app container to be centered properly

--- src/components/styles/AppContainer.styled.js
+++ src/components/styles/AppContainer.styled.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   width: 1200px;
-  height: 2000px;
-  position:absolute;
+  height: 1717px;
+  position: absolute; 
   background-color: #232323;
+  margin: auto;
+  left: 0;
+  right: 0;
 `
