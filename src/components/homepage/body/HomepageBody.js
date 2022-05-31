4f2fcdd48d4f7caebef6441d655ac579e4051c55
AlexAndrianov <aliaksei.andryianau@smartpension.co.uk>
2022-05-31 14:03:47 +0200

    added films count component

--- src/components/homepage/body/HomepageBody.js
+++ src/components/homepage/body/HomepageBody.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import MovieCard from './MovieCard'
+import MoviesFoundCounter from './MoviesFoundCounter'
 import SortingGroup from './SortingGroup'
 import { Films } from '../../../fixtures.js'
+import MooviesFoundCounter from './MoviesFoundCounter'
 
 export default function HomepageBody() {
   return (
     <>
       <SortingGroup />
+      <MoviesFoundCounter />
       {Films.map((film) => (
         <div key={film.id} className="movie-card">
           <MovieCard film_attrs={film}/>
---
+++ src/components/homepage/body/MoviesFoundCounter.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MoviesFoundCounter() {
+  return (
+    <div>39 movies found</div>
+  )
+}
